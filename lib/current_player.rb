bcc6d09a5f7e77178308a77c64762b86e1c59cef
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-07 05:21:41 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board1)
+
+    board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+    i=0
+    while i<8
+      if board1[i]=="X"|| board1[i]=="O"
+
+        puts "This is loop number #{i}"
+        i+=1
+    end
+
+
+   end
+
+
+
+end
