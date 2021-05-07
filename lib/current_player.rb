d2d222afbc36cf773a91c26c38a0b755d8ee212a
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-07 06:12:38 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,12 @@
-def turn_count(board1)
-  board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-  board1.count{|token| token=="X" || token=="O"}
+def turn_count(board)
+  
+  board.count{|token| token=="X" || token=="O"}
 end
-def turn_count(board2)
 
-  board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
-  board2.count{|token| token=="X" || token=="O"}
+turn_count(board)
 
 
-end
+
 def current_player(board)
   turn_count(board) % 2==0? "X": "O"
 
