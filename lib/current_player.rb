456465a83c4c9a8250e9ab6ece66100dac16e56d
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-07 06:15:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,7 +4,7 @@ def turn_count(board)
 
 end
 board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
-board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+board2 = ["O", " ", " ", " ", "X", " ", " ", "O", "X"]
 turn_count(board1)
 turn_count(board2)
 
