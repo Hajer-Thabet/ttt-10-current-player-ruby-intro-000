6e5008e9d7f2deb745803cceef23df519f50488c
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-07 05:24:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board1)
 
     board1 = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
     i=0
-    while i<8
+    
       if (board1[i]=="X")|| (board1[i]=="O")
 
 
@@ -11,7 +11,7 @@ def turn_count(board1)
        end
 
 
-     end
+  
 
 
 
